0392d3a7151b556f27bb01c8b8db26dcea75eec4
YKFD <chervonchuk316@gmail.com>
2021-06-16 03:50:59 +0300

    remove video

--- src/sockets/action.js
+++ src/sockets/action.js
@@ -11,12 +11,11 @@ module.exports = class SocketAction {
         console.info('Socket connected:', socket.id);
 
         const roomId = socket.handshake.query.roomId;
+        const userId = socket.handshake.query.userPeerId;
         const res = await userService.increaseCurrentUserCount(roomId);
         if (res) {
             socket.join(roomId);
-            socket.broadcast
-                .to(roomId)
-                .emit(events.JOIN_NEW_USER, socket.handshake.query.userPeerId);
+            socket.broadcast.to(roomId).emit(events.JOIN_NEW_USER, userId);
         } else {
             socket.emit(events.FAIL_TO_JOIN);
             return;
@@ -24,13 +23,16 @@ module.exports = class SocketAction {
         socket.on(events.SEND_MESSAGE_TO_SERVER, (event) =>
             onSendMessage(socket, event)
         );
-        socket.on(events.DISCONNECT, () => onDisconnect(socket, roomId));
+        socket.on(events.DISCONNECT, () =>
+            onDisconnect(socket, roomId, userId)
+        );
     }
 };
 
-const onDisconnect = async (socket, roomId) => {
+const onDisconnect = async (socket, roomId, userId) => {
     console.info('Socket disconnected:', socket.id);
     await userService.decrementCurrentUserCount(roomId);
+    socket.broadcast.to(roomId).emit(events.DISCONNECT_USER, userId);
 };
 
 const onSendMessage = async (socket, message) => {
--- src/sockets/events.js
+++ src/sockets/events.js
@@ -5,4 +5,5 @@ module.exports = Object.freeze({
     SEND_MESSAGE_TO_SERVER: 'send_message_to_server',
     SEND_MESSAGE_TO_USER: 'send_message_to_user',
     JOIN_NEW_USER: 'join_new_user',
+    DISCONNECT_USER: 'disconnect_user',
 });
